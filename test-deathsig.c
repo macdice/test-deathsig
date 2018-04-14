6e8db788e6c9cd10c25384229a2760a15218c553
Thomas Munro <munro@ip9.org>
2018-04-14 04:18:48 +0100

    Test SET and SET.

--- test-deathsig.c
+++ test-deathsig.c
@@ -7,28 +7,42 @@
 static void
 my_signal_handler(int signum)
 {
-	printf("Got signal %d!\n", signum);
+	printf("got signal %d\n", signum);
 }
 
 int main(int argc, char **argv)
 {
+	int signum;
         int rc = fork();
 
         if (rc < 0) {
                 perror("fork");
                 return 1;
         } else if (rc == 0) {
-		int signum = SIGINFO;
+		/* child */
 
+		/* request a signal on parent death and register a handler */
+		signum = SIGINFO;
 		signal(signum, my_signal_handler);
-		rc = procctl(P_PID, 0, 11, &signum);
-                printf("hello from child, procctl ret = %d, errno = %d\n", rc, errno);
+		rc = procctl(P_PID, 0, PROC_SET_PDEATHSIG, &signum);
+                printf("procctl(P_PID, 0, PROC_SET_PDEATHSIG, &signum) returned = %d, errno = %d\n", rc, errno);
+
+		/* check that we can read the signal number back */
+		signum = 0xdeadbeef;
+		rc = procctl(P_PID, 0, PROC_GET_PDEATHSIG, &signum);
+                printf("procctl(P_PID, 0, PROC_GET_PDEATHSIG, &signum) returned = %d, errno = %d, signum = %d\n", rc, errno, signum);
+
+		/* wait for parent to die and signal us... */
                 sleep(4);
+
                 printf("goodbye from child\n");
         } else {
-                printf("hello from parent, child pid = %d\n", rc);
+		/* parent */
+
+		/* wait for a short time to give the child time to request a signal */
                 sleep(2);
-                printf("goodbye from parent\n");
+
+		/* exit, should signal child */
         }
         
         return 0;

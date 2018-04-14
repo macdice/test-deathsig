5dac498e29e521cfa7df8e58b0988aa2c4b131f1
Thomas Munro <munro@ip9.org>
2018-04-14 01:48:36 +0100

    Show signal number.

--- test-deathsig.c
+++ test-deathsig.c
@@ -4,26 +4,28 @@
 #include <sys/procctl.h>
 #include <sys/signal.h>
 
+static void
+my_signal_handler(int signum)
+{
+	printf("Got signal %d!\n", signum);
+}
+
 int main(int argc, char **argv)
 {
         int rc = fork();
 
-        if (rc < 0)
-        {
+        if (rc < 0) {
                 perror("fork");
                 return 1;
-        }
-        else if (rc == 0)
-        {
+        } else if (rc == 0) {
 		int signum = SIGINFO;
 
+		signal(signum, my_signal_handler);
 		rc = procctl(P_PID, 0, 11, &signum);
                 printf("hello from child, procctl ret = %d, errno = %d\n", rc, errno);
                 sleep(4);
                 printf("goodbye from child\n");
-        }
-        else
-        {
+        } else {
                 printf("hello from parent, child pid = %d\n", rc);
                 sleep(2);
                 printf("goodbye from parent\n");
